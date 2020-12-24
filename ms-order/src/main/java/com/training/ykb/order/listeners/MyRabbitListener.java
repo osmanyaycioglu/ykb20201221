27d57313feba0eaccff86478206ffbbcab8bb24c
Osman <osman.yaycioglu@gmail.com>
2020-12-24 15:22:52 +0300

    day 4 7

--- ms-order/src/main/java/com/training/ykb/order/listeners/MyRabbitListener.java
+++ ms-order/src/main/java/com/training/ykb/order/listeners/MyRabbitListener.java
@@ -1,29 +1,26 @@
-package com.training.ykb;
+package com.training.ykb.order.listeners;
 
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRabbitListener {
 
 
-    @RabbitListener(bindings = @QueueBinding(value = @Queue(name = "notification_q",
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(name = "notification_res_q",
                                                             durable = "true",
                                                             autoDelete = "false"),
-                                             exchange = @Exchange(name = "notification_ex",
+                                             exchange = @Exchange(name = "notification_res_ex",
                                                                   durable = "true",
                                                                   autoDelete = "false",
                                                                   type = ExchangeTypes.DIRECT),
-                                             key = "notification_request"))
-    @SendTo("notification_res_ex/notification_reponse")
-    public String handle(final Notification notf) {
-        System.out.println(notf);
-        return "Request : " + notf + " --> processed...";
+                                             key = "notification_reponse"))
+    public void handle(final String response) {
+        System.out.println(response);
     }
 
 }
