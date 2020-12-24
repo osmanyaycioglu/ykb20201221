4d9ef075e92881aadaae60cb25d2b53882e6176d
Osman <osman.yaycioglu@gmail.com>
2020-12-24 16:32:55 +0300

    Day 4 8

--- ms-order/src/main/java/com/training/ykb/order/rest/OrderRest.java
+++ ms-order/src/main/java/com/training/ykb/order/rest/OrderRest.java
@@ -1,7 +1,10 @@
 package com.training.ykb.order.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,14 @@ import com.training.ykb.rest.error.RestException;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderRest {
 
     @Autowired
     private OrderService os;
+
+    @Value("${refresh.test}")
+    private String       testStr;
 
     @PostMapping("/place")
     public String place(@Validated @RequestBody final Order order) {
@@ -27,6 +34,11 @@ public class OrderRest {
     public String place2(@Validated @RequestBody final Order order) throws RestException {
         // Feign client
         return this.os.orderPlace2(order);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return this.testStr;
     }
 
 }
