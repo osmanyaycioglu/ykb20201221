6948e9c2e1b8ec21faff79637a65056ca8892035
Osman <osman.yaycioglu@gmail.com>
2020-12-24 12:21:22 +0300

    DAy 4 2

--- spring-project/src/main/java/com/training/ykb/SpringProjectApplication.java
+++ spring-project/src/main/java/com/training/ykb/SpringProjectApplication.java
@@ -2,8 +2,10 @@ package com.training.ykb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 //@SpringBootApplication(scanBasePackages = {
 //                                            "com.training.ykb",
 //                                            "a.b.c"
